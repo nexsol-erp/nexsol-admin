43d1b7018bc8a5b0b957f70464300b5c356e6802
regy george <regygeorge@gmail.com>
2024-08-31 23:01:21 +0530

    tocken added in get items

--- src/services/apiservice.js
+++ src/services/apiservice.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-
+const token = localStorage.getItem("jwtToken");
 const tenancyId = localStorage.getItem("tenancyId");
 const apiClient = axios.create({
-  baseURL: `/api/${tenancyId}`, 
+  baseURL: `/api/${tenancyId}`,
   headers: {
+    Authorization: `Bearer ${token}`,
     "Content-Type": "application/json",
   },
 });
